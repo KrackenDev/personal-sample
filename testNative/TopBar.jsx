7ddfb3c59a8a77d9ae2261afb7703b50148c59f2
JT <jt.mozingo@wsrb.com>
2024-04-11 16:54:52 -0700

    initial commit

---
+++ testNative/TopBar.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+
+import * as React from 'react';
+
+import {Appbar} from 'react-native-paper';
+
+const MyComponent = () => (
+  <Appbar.Header>
+    <Appbar.BackAction onPress={() => {}} />
+    <Appbar.Content title="Title" />
+    <Appbar.Action icon="calendar" onPress={() => {}} />
+    <Appbar.Action icon="magnify" onPress={() => {}} />
+  </Appbar.Header>
+);
+
+export default MyComponent;
---
+++ testNative/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import * as React from 'react';
+
+import App from './App';
+import {AppRegistry} from 'react-native';
+import {PaperProvider} from 'react-native-paper';
+import {name as appName} from './app.json';
+
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
